136a51fec9458dc0f3c762e36b2cce3f59ce9dce
Polina Nepochatova <54804662+Nepochatova-Polina@users.noreply.github.com>
2023-05-27 15:21:24 +0300

    Delete EpamFinalProject directory

--- EpamFinalProject/src/main/resources/SQL/Order.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists orders
-(
-    id              serial primary key,
-    cruise_id       bigint not null references cruises (id) on delete cascade,
-    user_id         bigint not null references users (id) on delete cascade,
-    status          varchar(255)
-);
-alter table orders
-    owner to "user";
